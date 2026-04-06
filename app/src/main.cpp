905d55fc3ae432d43d88eaefca0e3d3d0ffd77dc
Aravindhan Ashok <aravindsparkstar@gmail.com>
2026-04-06 18:05:36 +0100

    l3-task1 Kconfig feature
    Added LED Subsystem Kconfig to control
    Led blink time, brighness and Advance debug settings.
    
    Signed-off-by: Aravindhan Ashok <aravindsparkstar@gmail.com>

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,10 +2,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
+#define LED_DEFAULT_BLINK_TIME 3000
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -19,12 +19,20 @@ int main(void)
 
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
+#ifndef CONFIG_BLINK_SLEEP_TIME_MS
+        LOG_INF("Using defaut blink time: %u ms", LED_DEFAULT_BLINK_TIME);
+#endif
     while (1) {
         if (gpio_pin_toggle_dt(&led) < 0) return 0;
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+#if CONFIG_BLINK_SLEEP_TIME_MS
+        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+#else
+        k_msleep(LED_DEFAULT_BLINK_TIME);
+#endif
+
     }
     return 0;
 }

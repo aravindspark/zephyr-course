07aba260a862dba07e875e38901d1a1db1e0b46c
Aravindhan Ashok <aravindsparkstar@gmail.com>
2026-05-02 02:34:37 +0100

    l5-task2 Custom boardfrom scratch method
    Created pico2_scratch from scratch method
    
    Signed-off-by: Aravindhan Ashok <aravindsparkstar@gmail.com>

---
+++ app/boards/pico2_scratch/board.c
@@ -0,0 +1,8 @@
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(board_control, CONFIG_LOG_DEFAULT_LEVEL);
+
+void board_late_init_hook(void) {
+    LOG_INF("Board Initialized");
+}
